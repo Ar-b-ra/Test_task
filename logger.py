1789626752965cefd540296a5b4d5f49e7186319
Roman Arbuzov <lpandlipmbizkit@yandex.ru>
2023-10-11 21:53:13 +0300

    feat: добавлен логгер

---
+++ logger.py
@@ -0,0 +1,15 @@
+import logging
+
+
+file_log = logging.FileHandler("Log.log")
+console_out = logging.StreamHandler()
+
+logging.basicConfig(
+    handlers=(file_log, console_out),
+    format="[%(asctime)s | %(levelname)s]: %(message)s",
+    datefmt="%m.%d.%Y %H:%M:%S",
+    level=logging.INFO,
+)
+
+root_logger = logging.getLogger(__name__)
+root_logger.setLevel(logging.DEBUG)
