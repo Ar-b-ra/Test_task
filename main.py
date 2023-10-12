a69fd9c631a41fd2f487a174faa4558ef0a92b4b
Roman Arbuzov <lpandlipmbizkit@yandex.ru>
2023-10-12 21:40:28 +0300

    feat: доработана возможно получения ресурсов и их типов из БД

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from http.server import HTTPServer
 
-from logger import root_logger
+from db.db_resources import ResourcesDataBaseManager
+from logger import init_logger_levels
 from request_handler import HTTPRequestHandler
 
+test_db = "test_db"
+data_base = ResourcesDataBaseManager(dbname=test_db, user="postgres", password="password", host="localhost", port=5432)
+
 if __name__ == "__main__":
+    init_logger_levels()
+    data_base.prepare_test_db()
+    data_base.create_base_tables()
     host = "localhost"
     port = 8000
-
+    HTTPRequestHandler.db = data_base
     server = HTTPServer((host, port), HTTPRequestHandler)
-    root_logger.info(f"Сервер запущен на http://{host}:{port}...")
-
     server.serve_forever()
+
+
--- wsgi.py
+++ wsgi.py
@@ -1,19 +1,46 @@
+import json
 from wsgiref.simple_server import make_server
 
-from logger import root_logger
+from db.db_resources import ResourcesDataBaseManager
+from logger import root_logger, init_logger_levels
+
+test_db = "test_db"
+data_base = ResourcesDataBaseManager(dbname=test_db, user="postgres", password="password", host="localhost", port=5432)
 
 
 def application(environ, start_response):
     status = "200 OK"
     headers = [("Content-type", "text/plain; charset=utf-8")]
-
     start_response(status, headers)
     root_logger.info(headers)
-
-    return [b"Hello world, asdsad"]
+    path = environ.get("PATH_INFO", "/")
+    try:
+        request_body_size = int(environ.get('CONTENT_LENGTH', 0))
+    except ValueError:
+        request_body_size = 0
+    request_method = environ["REQUEST_METHOD"]
+    request_body = environ["wsgi.input"].read(request_body_size)
+    root_logger.info(request_body.decode("UTF-8"))
+    if path == "/resources":
+        if request_method == "GET":
+            return [str(data).encode("UTF-8") for data in data_base.get_resources()]
+        elif request_method == "POST":
+            prepared_json = json.loads(request_body)
+            root_logger.info(prepared_json)
+    elif environ["REQUEST_METHOD"] == "POST":
+        if path == "/":
+            return [b"Received POST request"]
+    elif environ["REQUEST_METHOD"] == "PATCH":
+        pass
+    return [b"404 Not Found"]
 
 
 if __name__ == "__main__":
-    with make_server("", 8000, application) as httpd:
-        root_logger.info("Сервер запущен на порту 8000...")
+    host = "localhost"
+    port = 8000
+    init_logger_levels()
+    data_base.prepare_test_db()
+    data_base.create_base_tables()
+    with make_server(host, port, application) as httpd:
+        root_logger.info(f"Сервер запущен на https://{host}:{port}...")
         httpd.serve_forever()
